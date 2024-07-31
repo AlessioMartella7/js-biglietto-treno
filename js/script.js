9d0dc1811f500c694146811a0c9086b6d7e63cf4
AlessioMartella7 <alessio.mart87@gmail.com>
2024-07-31 15:27:49 +0200

    added schedule

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
-*Nota
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+/* 
+1-recupero gli elementi di mio interesse dal DOM.
+2-creo una costante prezzo/km.
+3-creo una costante per lo sconto del 20%.
+4-creo una costante per lo sconto del 40%.
+5-chiedo all'utente i km che vuole percorrere.
+6-chiedo all'utente la sua età.
+7-calcolo il prezzo in base ai kilometri.
+8-applico uno sconto se possibile.
+9-stampo il documento.
 */
